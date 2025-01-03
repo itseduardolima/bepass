ea6f874a4bb61ae6113fd650be1cbadb8cc9fde1
itseduardolima <eduardolima2417@gmail.com>
2025-01-03 12:58:02 -0400

    feat: animate FAQ section

--- components/sections/FAQ.tsx
+++ components/sections/FAQ.tsx
@@ -1,25 +1,44 @@
 "use client";
 
-import { useTranslations } from 'next-intl';
+import { useRef } from "react";
+import { useTranslations } from "next-intl";
+import { motion, useInView } from "framer-motion";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import { faqData } from '@/mock/faq';
+} from "@/components/ui/accordion";
+import { faqData } from "@/mock/faq";
 
-
-
-export default function FAQSection() {
-  const t = useTranslations('faq');
+export default function FAQ() {
+  const t = useTranslations("faq");
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: true, amount: 0.3 });
 
   return (
-    <section id="contact" className="py-24 px-4 max-w-7xl mx-auto">
-      <h2 className="text-4xl md:text-5xl font-semibold text-primary mb-12 text-center">
-        {t('title')}
-      </h2>
-      <div className="grid md:grid-cols-2 md:gap-8">
+    <motion.section
+      id="contact"
+      ref={sectionRef}
+      className="py-24 px-4 max-w-7xl mx-auto"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.h2
+        className="text-4xl md:text-5xl font-semibold text-primary mb-12 text-center"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: isInView ? 0 : 50, opacity: isInView ? 1 : 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        {t("title")}
+      </motion.h2>
+      <motion.div
+        className="grid md:grid-cols-2 gap-8"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: isInView ? 0 : 50, opacity: isInView ? 1 : 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+      >
         <Accordion type="single" collapsible className="w-full">
           {faqData.slice(0, 4).map((item, index) => (
             <AccordionItem key={index} value={`item-${index}`}>
@@ -44,7 +63,7 @@ export default function FAQSection() {
             </AccordionItem>
           ))}
         </Accordion>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 }
