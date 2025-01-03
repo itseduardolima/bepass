74ba820eea0ed60700b3b081606963325ec24875
itseduardolima <eduardolima2417@gmail.com>
2025-01-03 10:52:29 -0400

    feat: custom scroll

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -20,7 +20,7 @@ export default async function LocaleLayout({
   const messages = await getMessages();
  
   return (
-    <html lang={locale}>
+    <html lang={locale} className="custom-scroll">
        <body>
         <NextIntlClientProvider messages={messages}>
           <CurrencyProvider>
